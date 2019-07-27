1289ee4dcf32911cc66f6681913dc60a7b33bd3f
Mitsuki Watanabe <ix5231if@gmail.com>
2019-07-27 21:07:27 +0900

    Initial commit!

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function index() {
+    return (
+        <div>
+            <Head>
+                <title>Drawer</title>
+            </Head>
+            <p>ここには何もないよ</p>
+
+            <a href="https://mag.ix5231.xyz">磁束のアレ</a>
+        </div>
+    );
+}
