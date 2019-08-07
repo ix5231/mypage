f451127baf61cf75afd397ca8d6ac2e77a83786c
Mitsuki Watanabe <ix5231if@gmail.com>
2019-08-07 19:08:49 +0900

    Move to TypeScript

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Index = () =>
+    <div>
+        <Head>
+            <title>Drawer</title>
+        </Head>
+        <p>ここには何もないよ</p>
+
+        <a href="https://mag.ix5231.xyz">磁束のアレ</a>
+    </div>;
+
+export default Index;
