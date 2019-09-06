0d81bbe0efd34db027929452099556c7f82d6713
Mitsuki Watanabe <ix5231if@gmail.com>
2019-09-06 15:46:13 +0900

    Resolve deployment issue

--- next.config.js
+++ next.config.js
@@ -1,2 +1,37 @@
 const withCSS = require('@zeit/next-css');
-module.exports = withCSS({});
+
+const withAssetRelocator = (nextConfig = {}) => {
+    return Object.assign({}, nextConfig, {
+      webpack(config, options) {
+        const { isServer } = options
+  
+        if (isServer) {
+          config.node = Object.assign({}, config.node, {
+            __dirname: false,
+            __filename: false,
+          })
+  
+          config.module.rules.unshift({
+            test: /\.(m?js|node)$/,
+            parser: { amd: false },
+            use: {
+              loader: '@zeit/webpack-asset-relocator-loader',
+              options: {
+                outputAssetBase: 'assets',
+                existingAssetNames: [],
+                wrapperCompatibility: true,
+                escapeNonAnalyzableRequires: true,
+              },
+            },
+          })
+        }
+  
+        if (typeof nextConfig.webpack === 'function') {
+          return nextConfig.webpack(config, options)
+        }
+        return config
+      },
+    })
+  }
+
+module.exports = withAssetRelocator(withCSS({}));
