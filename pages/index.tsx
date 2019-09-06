83e83fed3cabf838a5859e3b42ded005162bb63f
Mitsuki Watanabe <ix5231if@gmail.com>
2019-09-06 15:49:26 +0900

    Implement counter (#2)
    * Add firebase to dependencies
    
    * Implement Counter
    
    * Resolve deployment issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,45 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import firestore from '../src/api';
+import firebase from 'firebase/app';
 
-const Index = () =>
-    <div>
+function addCount(): Promise<void> {
+    const increment = firebase.firestore.FieldValue.increment(1);
+    return firestore!.collection("global").doc("access-counter").update({ toppage: increment });
+}
+
+const Index = () => {
+    const [accessCount, setAccessCount] = useState("Loading...");
+    
+    useEffect(() => {
+        return firestore!
+            .collection("global")
+            .doc("access-counter")
+            .onSnapshot((doc) => {
+                setAccessCount(doc.get('toppage'));
+            });
+    }, [])
+
+    return <div>
         <Head>
-            <title>Drawer</title>
+            <title>Diminished 2nd</title>
         </Head>
-        <p>ここには何もないよ</p>
 
-        <a href="https://mag.ix5231.xyz">磁束のアレ</a>
+        <article id="access-counter">
+            <p>手動アクセスカウンター: {accessCount}</p>
+            <button onClick={
+                () => {
+                    setAccessCount("Loading...");
+                    addCount();
+                }
+            } >Click Here!</button>
+        </article>
+
+        <article id="contents">
+            <h1>おしながき</h1>
+            <a href="https://mag.ix5231.xyz">磁束のアレ</a>
+        </article>
     </div>;
+}
 
 export default Index;
